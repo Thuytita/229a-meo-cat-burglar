bfc82a8e15a8660b45695c5e19cc41dab43f7bb5
tatah2546 <surachet.tong@bumail.net>
2024-03-20 22:00:58 +0700

    update movement and char rotation

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -14,7 +14,8 @@ public class PlayerMovement : MonoBehaviour
 
     public bool groundCheck = false;
     public float groundCheckDistance;
-    public float bufferCheckDistance = 0.1f;
+    public float Myfloat;
+    
     
     
     // Start is called before the first frame update
@@ -34,15 +35,21 @@ public class PlayerMovement : MonoBehaviour
         force = mass * acceleration;
 
         Jump();
+        SpeedControl();
         
         
         Vector3 moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         moveDirection.Normalize();
         
         
-        //rb.AddForce(move * 5,ForceMode.Force);
-        //rb.MovePosition(transform.position+(move*0.1f));
-        transform.Translate((moveDirection * force *Time.deltaTime));
+        if (moveDirection.magnitude >= 0.1f)
+        {
+            float Angle = Mathf.Atan2(moveDirection.x , moveDirection.z) * Mathf.Rad2Deg; 
+            float Smooth = Mathf.SmoothDampAngle(transform.eulerAngles.y , Angle , ref Myfloat , 0.1f); 
+            transform.rotation = Quaternion.Euler(0,Smooth,0);
+        }
+        
+        rb.AddForce(moveDirection * force, ForceMode.Force);
         
 
         
@@ -53,6 +60,19 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
         }
+    }
+    
+    private void SpeedControl()
+    {
+        Vector3 flatvel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        //limit velocity 
+        if (flatvel.magnitude > force)
+        {
+            Vector3 limitVel = flatvel.normalized * force;
+            rb.velocity = new Vector3(limitVel.x, rb.velocity.y, limitVel.z);
+        }
+        Debug.Log(rb.velocity);
+
     }
 
     

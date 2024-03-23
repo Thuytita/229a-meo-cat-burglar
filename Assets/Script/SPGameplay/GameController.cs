49a196d4568a694a600b768e58a7c5f407f45c7f
Thuytita <thuytita.sric@bumail.net>
2024-03-23 23:01:48 +0700

    Restart game and addui

--- Assets/Script/SPGameplay/GameController.cs
+++ Assets/Script/SPGameplay/GameController.cs
@@ -4,32 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
-    public int totalItems; // Total number of items in the collection
-    public int itemsCollected; // Current number of items collected by the player
-    
-
-    // Update UI to reflect current collection progress
-    void UpdateUI()
+    public void RestartGame()
     {
-        // Update UI elements such as progress bars, text displays, etc.
-    }
-
-    // Call this method whenever an item is collected
-    public void CollectItem()
-    {
-        itemsCollected++;
-        UpdateUI();
-
-        // Check if all items are collected
-        if (itemsCollected >= totalItems)
-        {
-            Credit();
-        }
-    }
-
-    // Trigger end game sequence
-    void Credit()
-    {
-        SceneManager.LoadSceneAsync(2);
+        SceneManager.LoadSceneAsync(1);
     }
 }
